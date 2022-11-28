b7ec86b3b79252eaf794eea70401572eeae76cb4
jmdvries <jona.de.vries@gmail.com>
2022-11-28 13:05:06 +0100

    opslaan werkt, inladen niet, poging #2

--- src/GA_dino.py
+++ src/GA_dino.py
@@ -17,11 +17,25 @@ num_parents_mating = 4
 pop_size = (sol_per_pop, num_weights)  # The population will have sol_per_pop chromosome where each chromosome has
 # num_weights genes.
 # Creating the initial population.
-new_population = json.load("in.json","r")
-if new_population is not None:
-    new_population = numpy.random.uniform(low=-4.0, high=4.0, size=pop_size)
-print(new_population)
+new_population = numpy.load(str(open("pop_gen_1.npy", "r")))
+# new_population = numpy.random.uniform(low=-4.0, high=4.0, size=pop_size)
 
+"""
+# Loop over population, extracting data
+pop_list = []
+for org in new_population:
+    row = []
+    for w in org:
+        row.append(w)
+    pop_list.append(row)
+
+# Write list data to file
+with open('jpop_gen_0.json', "w") as f:
+    json.dump(pop_list, f)
+
+# Dump numpy-object directly to file (can be loaded directly again)
+numpy.save('pop_gen_0', new_population, allow_pickle=False, fix_imports=False)
+"""
 
 num_generations = 1
 for generation in range(num_generations):
@@ -31,10 +45,12 @@ for generation in range(num_generations):
 
     fitness = ga.cal_pop_fitness(new_population)
 
-    # dump the best solution into file
-    # file =
-    new_gen = dict(enumerate(new_population))
-    json.dump(new_gen, open("out.json", "w"))
+    fitness_list = []
+    for score in fitness:
+        fitness_list.append(score)
+
+    with open('jfitness_gen_0.json', "w") as f:
+        json.dump(fitness_list, f)
 
     # Selecting the best parents in the population for mating.
     print(f'# Selecting the best parents in the population for mating.')
@@ -52,16 +68,31 @@ for generation in range(num_generations):
     new_population[0:parents.shape[0], :] = parents
     new_population[parents.shape[0]:, :] = offspring_mutation
 
+    # Loop over population, extracting data
+    pop_list = []
+    for org in new_population:
+        row = []
+        for w in org:
+            row.append(w)
+        pop_list.append(row)
+
+    # Write list data to file
+    with open('jpop_gen_2.json', "w") as f:
+        json.dump(pop_list, f)
+
+    # Dump numpy-object directly to file (can be loaded directly again)
+    numpy.save('pop_gen_2', new_population, allow_pickle=False, fix_imports=False)
+
     # The best result in the current iteration.
-    print("Best result : ", numpy.max(fitness))
-    print(new_population)
+    # print("Best result : ", numpy.max(fitness))
+    # print(new_population)
 
 # Getting the best solution after iterating finishing all generations.
 # At first, the fitness is calculated for each solution in the final generation.
-#TODO: (optioneel) voorkomen dat we opnieuw de fitness moeten berekenen voor elke oplossing, duurt nml wat lang
-fitness = ga.cal_pop_fitness(new_population)
+# TODO: (optioneel) voorkomen dat we opnieuw de fitness moeten berekenen voor elke oplossing, duurt nml wat lang
+# fitness = ga.cal_pop_fitness(new_population)
 # Then return the index of that solution corresponding to the best fitness.
-best_match_idx = numpy.where(fitness == numpy.max(fitness))
+# best_match_idx = numpy.where(fitness == numpy.max(fitness))
 
-print("Best solution : ", new_population[best_match_idx])
-print("Best solution fitness : ", int(numpy.max(fitness)))
+# print("Best solution : ", new_population[best_match_idx])
+# print("Best solution fitness : ", int(numpy.max(fitness)))
--- src/dinogame/perceptron.py
+++ src/dinogame/perceptron.py
@@ -1,12 +1,13 @@
 import random
-n_input=3 #number of inputs & weights
-weights = [random.uniform(-1,1) for x in range(n_input)] #list of weights
-inputs = [1]+[int(input("input?")) for x in range(n_input-1)]#how to automate input? 
+n_input=3 # number of inputs & weights
+weights = [random.uniform(-1, 1) for x in range(n_input)]  # list of weights
+inputs = [1]+[int(input("input?")) for x in range(n_input-1)]
 
-#print(weights)
+# print(weights)
 print(inputs)
 
-#activation function
+
+# activation function
 def sign(n):
     if n >= 0:
         return 1
@@ -14,13 +15,12 @@ def sign(n):
         return -1
 
 
-
 class Perceptron:
-    def _init_(self, n_input, func): #extra argument: actfunc?
+    def _init_(self, n_input, func):
         self.n_input = n_input
         self.activation_function = func 
        
-#make a guess based on input and weight
+# make a guess based on input and weight
     def guess(self, inputs):
         Sum = 0.0
         for i in range(self.n_input):
@@ -28,11 +28,9 @@ class Perceptron:
         # output = sign(Sum)
         output = self.activation_function()
         print(output)
-        
 
     guess(inputs)
             
 
-
-p = Perceptron(2, sign )
-p.guess()
+p = Perceptron()
+p.guess(inputs)
